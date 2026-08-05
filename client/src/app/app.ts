2eba69f13567ca33f2527cc3e1708dda485a1cd0
Abhilash Kuvalkar <abhilashkuvalkar@live.com>
2026-08-05 22:29:47 +0530

    End of section 5

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,25 +1,39 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
+import { Nav } from "../layout/nav/nav";
+import { User } from '../types/user';
+import { AccountService } from '../core/services/account-service';
+import { Home } from "../features/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Nav, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
   protected readonly http = inject(HttpClient);
+  protected readonly accountService = inject(AccountService);
+
   protected readonly title = signal('client');
-  protected members = signal<any>([]);
+  protected members = signal<User[]>([]);
 
   async ngOnInit() {
     this.members.set(await this.getMembers());
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem("user");
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
   }
 
   async getMembers() {
     try {
-      return lastValueFrom(this.http.get("http://localhost:5185/api/members"));
+      return lastValueFrom(this.http.get<User[]>("http://localhost:5185/api/members"));
     } catch (error) {
       console.error(error);
       throw error;
