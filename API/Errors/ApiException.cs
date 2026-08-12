c5010442444673d2f64a765dd08f8b80727186a1
Abhilash Kuvalkar <abhilashkuvalkar@live.com>
2026-08-12 22:31:28 +0530

    End of section 7

---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Errors;
+
+public class ApiException(int statusCode, string message, string? description)
+{
+    public int StatusCode { get; set; } = statusCode;
+
+    public string Message { get; set; } = message;
+
+    public string? Description { get; set; } = description;
+}
---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+using API.Errors;
+
+namespace API.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionMiddleware> _logger;
+    private readonly IHostEnvironment _hostEnvironment;
+
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment hostEnvironment)
+    {
+        _next = next;
+        _logger = logger;
+        _hostEnvironment = hostEnvironment;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, "{Message}", exception.Message);
+
+            httpContext.Response.ContentType = "application/json";
+            httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            var response = _hostEnvironment.IsDevelopment()
+                ? new ApiException(httpContext.Response.StatusCode, exception.Message, exception.StackTrace)
+                : new ApiException(httpContext.Response.StatusCode, exception.Message, "Internal Server Error");
+
+            var options = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            var json = JsonSerializer.Serialize(response, options);
+            await httpContext.Response.WriteAsync(json);
+        }
+    }
+}
